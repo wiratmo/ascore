333092c2498d64099c263c6db91c057c097d79db
wiratmo <4a.wiratmo@gmail.com>
2019-03-15 18:25:02 +0700

    15march2019

--- train.py
+++ train.py
@@ -2,6 +2,12 @@ class ascore(object):
 
 	# import package
 	import pandas as pd
+	import numpy as np
+	import nltk
+	nltk.download('stopwords')
+	import re
+	from nltk.corpus import stopwords
+	from gensim.models import Word2Vec
 	
 	def __init__(self, answer, question):
 		super(ascore, self).__init__()
@@ -21,8 +27,18 @@ class ascore(object):
 		yScore = dAnswer.loc[:, ['Score']]
 		return xAnswer, xQuestion, xTAnswer, yScore
 
-	
+	def essayToWordList(self, sentences, remove_stopwords = True):
+		sentences = self.re.sub("[^a-zA-Z0-9/]"," ", sentences)
+		words = sentences.lower().split()
+		if remove_stopwords:
+			stops = set(P.stopwords.words("indonesian"))
+			words = [w for w in words if not w in stops]
+		return (words)
 
 P = ascore(answer='DataAnswerExam_SMP.csv', question='DataQuestionExam_SMP.csv')
 a, b, c, d = P.splitIO()
-print(len(d))		
+sentences = []
+for a1 in a.loc[:,['Answer']].values:
+	sentences.append(P.essayToWordList(a1[0]))
+print(len(sentences))
+print(sentences[350])
