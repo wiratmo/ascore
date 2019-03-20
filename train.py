f6925c21d283f25f19daad7ead03b24aeb7d2466
wiratmo <4a.wiratmo@gmail.com>
2019-03-20 07:16:34 +0700

    minor update

--- train.py
+++ train.py
@@ -7,15 +7,15 @@ from module.toWordList import toWordList
 from module.steamingWiki import steamingWiki
 from module.makeModelGensim import makeModelGensim
 
-wikiSource		= 'enwiki'
-answerData 		= 'DataAnswerExam_SMP.csv'
-questionData 	= 'DataQuestionExam_SMP.csv'
-dirData 		= cwd+'/data/'
-corpusInput		= wikiSource+'.bz2'
-wikiOutput		= wikiSource+'.txt'
-trainingAlgoritm= 0
-numDimension	= 200
-modelOutput		= wikiSource+'_word2vec_'+str(numDimension)+'.model'
+wikiSource			= 'enwiki'
+answerData			= 'DataAnswerExam_SMP.csv'
+questionData		= 'DataQuestionExam_SMP.csv'
+dirData				= cwd+'/data/'
+corpusInput			= wikiSource+'.bz2'
+wikiOutput			= wikiSource+'.txt'
+trainingAlgoritm	= 0
+numDimension		= 200
+modelOutput			= wikiSource+'_word2vec_'+str(numDimension)+'.model'
 
 
 a, b, c, d = importData(answer= dirData+answerData, question= dirData+questionData).ExposeData()
